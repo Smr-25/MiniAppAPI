f04fae57c71b017353ab41d3a655b0d7cd346087
Smr-25 <samiraa@code.edu.az>
2026-05-13 09:24:59 +0400

    13.05.26 commit

--- MiniAppApi/Program.cs
+++ MiniAppApi/Program.cs
@@ -2,21 +2,14 @@ using MiniAppApi;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//Add services to the container.
 var configuration = builder.Configuration;
 
 builder.Services.AddServices(configuration, builder);
 
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.MapOpenApi();
 app.MapScalarApiReference();
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthorization();
